1d4a82a1f4a75603671c6c61121fbedd2b14d53f
takeshiemoto <private.takeshiemoto@gmail.com>
2024-06-18 17:56:16 +0900

    feat: setup auth

--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => env('web'),
+        'passwords' => env('users'),
     ],
 
     /*
@@ -40,6 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'provider' => 'users',
     ],
 
     /*
@@ -62,7 +63,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => env(App\Models\User::class),
         ],
 
         // 'users' => [
@@ -93,7 +94,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'table' => env('password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
