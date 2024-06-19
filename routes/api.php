643378e4db3768df8cf09de8141e0f34a6fc1d82
takeshiemoto <private.takeshiemoto@gmail.com>
2024-06-19 17:28:49 +0900

    fix: api response

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use App\Http\Controllers\TodoController;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/me', [AuthController::class, 'me']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Apply auth:sanctum middleware to all /todos routes
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/todos', [TodoController::class, 'index']);
     Route::post('/todos', [TodoController::class, 'store']);
     Route::get('/todos/{id}', [TodoController::class, 'show']);
     Route::put('/todos/{id}', [TodoController::class, 'update']);
     Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
+
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
