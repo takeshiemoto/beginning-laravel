1ef9267eb5c090e3e7f2dee95946e70cd073092e
takeshiemoto <private.takeshiemoto@gmail.com>
2024-06-19 23:16:25 +0900

    feat: add user resource class

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Resources\AuthResource;
+use App\Http\Resources\UserResource;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -14,24 +17,19 @@ class AuthController extends Controller
         $credentials = $request->validated();
         if (Auth::guard('web')->attempt($credentials)) {
             $token = Auth::user()->createToken('AccessToken')->plainTextToken;
-            return response()->json(['token' => $token]);
+            return response()->json(new AuthResource($token), Response::HTTP_CREATED);
         }
         throw new AuthenticationException();
     }
 
     public function me(Request $request)
     {
-        return response()->json(
-            [
-                $request->user()->name,
-                $request->user()->email,
-            ]
-        );
+        return response()->json(new UserResource($request->user()), Response::HTTP_OK);
     }
 
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
-        return response()->json(['message' => 'logout']);
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
