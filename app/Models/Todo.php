f7da25f17f569f8ce3c797a05c7cd9be3ed8c39f
Tekeshi Emoto <45647252+takeshiemoto@users.noreply.github.com>
2024-07-22 22:53:18 +0900

    dev (#9)

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property bool $completed
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $user_id
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereCompleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Todo extends Model
 {
     use HasFactory;
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,8 @@
 <?php
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
+    IdeHelperServiceProvider::class,
 ];
